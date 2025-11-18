1c2b707194445b12afa1df0ba058804520f50afc
moonformeli <mgyang95@gmail.com>
2025-11-18 11:15:11 +0900

    refactor: Dto 파일 이름 변경

--- src/main/java/com/example/news/news_letter_back/controller/SubscriberController.java
+++ src/main/java/com/example/news/news_letter_back/controller/SubscriberController.java
@@ -1,16 +1,12 @@
 package com.example.news.news_letter_back.controller;
 
-import com.example.news.news_letter_back.dto.SubscriberDto;
 import com.example.news.news_letter_back.dto.SubscriberPageResponse;
-import com.example.news.news_letter_back.dto.SubscriberRequest;
-import com.example.news.news_letter_back.repository.SubscriberRepository;
+import com.example.news.news_letter_back.dto.SubscriberRequestDto;
 import com.example.news.news_letter_back.service.SubscriberService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-
-import java.util.List;
 
 @RestController// JSON 형태로 데이터로 받고 응답함
 @RequestMapping("/api/v1")
@@ -35,7 +31,7 @@ public class SubscriberController {
     }
 
     @PostMapping("/subscribe")
-    public ResponseEntity<?> subscribe(@RequestBody SubscriberRequest request) {
+    public ResponseEntity<?> subscribe(@RequestBody SubscriberRequestDto request) {
         return service.subscribe(request.getEmail());
     }
 
--- src/main/java/com/example/news/news_letter_back/dto/UnsubscriberRequestDto.java
+++ src/main/java/com/example/news/news_letter_back/dto/UnsubscriberRequestDto.java
@@ -5,6 +5,6 @@ import lombok.Setter;
 
 @Getter
 @Setter
-public class SubscriberRequest {
+public class UnsubscriberRequestDto {
     private String email;
 }
