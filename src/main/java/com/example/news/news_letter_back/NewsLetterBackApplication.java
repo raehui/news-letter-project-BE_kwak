2d8cf42b63052270ccd1cfe71a6ee8ec11e5d407
KWAK <glasofk12345@naver.com>
2025-12-09 12:01:33 +0900

    첫 번째 커밋

--- src/main/java/com/example/news/news_letter_back/NewsLetterBackApplication.java
+++ src/main/java/com/example/news/news_letter_back/NewsLetterBackApplication.java
@@ -8,6 +8,8 @@ public class NewsLetterBackApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(NewsLetterBackApplication.class, args);
+
+        // AWS 모니터링 환경을 설정하고 있습니다. 끝까지 파이팅 하세용!
     }
 
 }
