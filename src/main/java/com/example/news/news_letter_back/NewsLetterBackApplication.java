52aeefbc2c927eeb3c30d2bf9837d424e64f04d9
KWAK <glasofk12345@naver.com>
2025-12-13 01:02:32 +0900

    CI/CD 파이으라인 테스트

--- src/main/java/com/example/news/news_letter_back/NewsLetterBackApplication.java
+++ src/main/java/com/example/news/news_letter_back/NewsLetterBackApplication.java
@@ -9,7 +9,7 @@ public class NewsLetterBackApplication {
     public static void main(String[] args) {
         SpringApplication.run(NewsLetterBackApplication.class, args);
 
-        // AWS 모니터링 환경을 설정하고 있습니다. 끝까지 파이팅 하세용!
+        // AWS 모니터링 환경을 설정하고 있습니다. 끝까지 파이팅 하세용!!!!
     }
 
 }
